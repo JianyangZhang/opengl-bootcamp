3698caa05fafeeebd1ee42a27aedc0c591eed98a
Jianyang Zhang <jianyang212@gmail.com>
2017-08-15 18:29:23 +0800

    reset env in vs
    reset env in vs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include <GL/glew.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void pause();
+
+int main(int argc, char** argv) {
+	std::cout << "Hello World!" << std::endl;
+	pause();
+	return 0;
+}
+
+void pause() {
+	std::cout << std::endl << "Press any key to continue...";
+	getchar();
 }
