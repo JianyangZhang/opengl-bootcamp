a7f53a1c1ed0674552e9a3d0b8ab79f9cc02f265
Jianyang Zhang <jianyang212@gmail.com>
2017-08-17 00:25:39 +0800

    first opengl window
    set up env, open first window

--- display.h
+++ display.h
@@ -2,24 +2,26 @@
 #define DISPLAY_INCLUDED_H
 
 #include <string>
-#include <SDL2\SDL.h>
+#include <SDL2/SDL.h>
 
-class Display
-{
+using namespace std;
+
+class Display {
 public:
-	Display(int width, int height, const std::string& title);
-
-	void Clear(float r, float g, float b, float a);
-	void SwapBuffers();
-
+	Display(int width, int height, const string& title);
+	void clear(float r, float g, float b, float a);
+	void update();
+	bool isClosed();
 	virtual ~Display();
 protected:
 private:
 	void operator=(const Display& display) {}
 	Display(const Display& display) {}
-
 	SDL_Window* m_window;
 	SDL_GLContext m_glContext;
+	bool m_isClosed;
+	int INITIAL_POS_X = 25;
+	int INITIAL_POS_Y = 50;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -2,16 +2,16 @@
 #include <GL\glew.h>
 #include "display.h"
 #include "shader.h"
-#include <SDL2\SDL.h>
+#include <SDL2/SDL.h>
 #undef main
 
 using namespace std;
 
 int main(int argc, char** argv) {
-	Display display(800, 600, "Hello World");
-	Shader shader("./res/basicShader");
-	cout << "Hello World" << endl;
-	bool isRunning = true;
-	display.Clear(0.0f, 0.0f, 0.0f, 1.0f);
+	Display display(1366, 768, "OpenGL Window");
+	while (!display.isClosed()) {
+		display.clear(0.0f, 0.15f, 0.3f, 1.0f);
+		display.update();
+	}
 	return 0;
 }
