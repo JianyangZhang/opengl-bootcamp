0bee4f9c055777c843f11988214b8c6ae818cf18
Jianyang Zhang <jianyang212@gmail.com>
2017-08-25 11:50:06 +0800

    Display module and VS setup
    Display module and VS setup

---
+++ Display module and VS setup/rend.cpp
@@ -0,0 +1,128 @@
+#include	"stdafx.h"
+#include	"stdio.h"
+#include	"math.h"
+#include	"Gz.h"
+#include	"rend.h"
+/*   CS580 HW   */
+#include    "stdafx.h"  
+#include	"Gz.h"
+
+
+GzRender::GzRender(int xRes, int yRes) {
+/* HW1.1 create a framebuffer for MS Windows display:
+ -- set display resolution
+ -- allocate memory for framebuffer : 3 bytes(b, g, r) x width x height
+ -- allocate memory for pixel buffer
+ */
+	xres = xRes;
+	yres = yRes;
+	framebuffer = (char*) malloc(sizeof(GzPixel) * xRes * yRes);
+	pixelbuffer = (GzPixel*) malloc(sizeof(GzPixel) * xRes * yRes);
+}
+
+GzRender::~GzRender() {
+/* HW1.2 clean up, free buffer memory */
+	free(pixelbuffer);
+	free(framebuffer);
+}
+
+int GzRender::GzDefault() {
+/* HW1.3 set pixel buffer to some default values - start a new frame */
+	if (pixelbuffer == NULL) {
+		return GZ_FAILURE;
+	}
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			pixelbuffer[ARRAY(i, j)].blue = 1000;
+			pixelbuffer[ARRAY(i, j)].green = 1000;
+			pixelbuffer[ARRAY(i, j)].red = 1000;
+			pixelbuffer[ARRAY(i, j)].alpha = 1;
+			pixelbuffer[ARRAY(i, j)].z = 0;
+		}
+	}
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzPut(int i, int j, GzIntensity r, GzIntensity g, GzIntensity b, GzIntensity a, GzDepth z) {
+/* HW1.4 write pixel values into the buffer */
+	if (pixelbuffer == NULL || i < 0 || j < 0 || i >= xres || j >= yres) {
+		return GZ_FAILURE;
+	}
+
+	b = (b > 0) ? b : 0;
+	g = (g > 0) ? g : 0;
+	r = (r > 0) ? r : 0;
+	b = (b < 4095) ? b : 4095;
+	g = (g < 4095) ? g : 4095;
+	r = (r < 4095) ? r : 4095;
+
+	pixelbuffer[ARRAY(i, j)].blue = b;
+	pixelbuffer[ARRAY(i, j)].green = g;
+	pixelbuffer[ARRAY(i, j)].red = r;
+	pixelbuffer[ARRAY(i, j)].alpha = a;
+	pixelbuffer[ARRAY(i, j)].z = z;
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzGet(int i, int j, GzIntensity* r, GzIntensity* g, GzIntensity* b, GzIntensity* a, GzDepth* z) {
+/* HW1.5 retrieve a pixel information from the pixel buffer */
+	if (pixelbuffer == NULL || i < 0 || j < 0 || i >= xres || j >= yres) {
+		return GZ_FAILURE;
+	}
+
+	*b = pixelbuffer[ARRAY(i, j)].blue;
+	*g = pixelbuffer[ARRAY(i, j)].green;
+	*r = pixelbuffer[ARRAY(i, j)].red;
+	*a = pixelbuffer[ARRAY(i, j)].alpha;
+	*z = pixelbuffer[ARRAY(i, j)].z;
+
+	return GZ_SUCCESS;
+}
+
+
+int GzRender::GzFlushDisplay2File(FILE* outfile) {
+/* HW1.6 write image to ppm file -- "P6 %d %d 255\r" */
+	if (pixelbuffer == NULL || outfile == NULL) {
+		return GZ_FAILURE;
+	}
+
+	fprintf(outfile, "P6 %d %d 255\r", xres, yres);
+	char r, g, b;
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			r = pixelbuffer[ARRAY(i, j)].red >> 4;
+			g = pixelbuffer[ARRAY(i, j)].green >> 4;
+			b = pixelbuffer[ARRAY(i, j)].blue >> 4;
+			fprintf(outfile, "%c%c%c", r, g, b);
+		}
+	}
+	return GZ_SUCCESS;
+}
+
+int GzRender::GzFlushDisplay2FrameBuffer() {
+/* HW1.7 write pixels to framebuffer:
+	- put the pixels into the frame buffer
+	- CAUTION: when storing the pixels into the frame buffer, the order is blue, green, and red
+	- NOT red, green, and blue !!!
+*/
+	if (pixelbuffer == NULL || framebuffer == NULL) {
+		return GZ_FAILURE;
+	}
+
+	char b, g, r;
+	for (int i = 0; i < xres; i++) {
+		for (int j = 0; j < yres; j++) {
+			b = pixelbuffer[ARRAY(i, j)].blue >> 4;
+			g = pixelbuffer[ARRAY(i, j)].green >> 4;
+			r = pixelbuffer[ARRAY(i, j)].red >> 4;
+			framebuffer[ARRAY(i, j) * 3] = b;
+			framebuffer[ARRAY(i, j) * 3 + 1] = g;
+			framebuffer[ARRAY(i, j) * 3 + 2] = r;
+		}
+	}
+
+	return GZ_SUCCESS;
+}
